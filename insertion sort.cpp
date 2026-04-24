8240875a1fb6463b200462120ca92fd1a7cfe10a
Sushil raj <sushilrajr288@gmail.com>
2026-04-24 14:32:22 +0530

    Add files via upload

---
+++ insertion sort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 3};
+    int n = 5;
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
